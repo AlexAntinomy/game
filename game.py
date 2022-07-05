c86e1d780be8c332d36daa8215060afea8b1d453
Antinomy <alexksa179@gmail.com>
2022-07-05 16:48:35 +0300

    starting

---
+++ game.py
@@ -0,0 +1,12 @@
+
+from random import randint
+print("Угадай где привидение")
+feeling_brave = True
+score = 0
+while feeling_brave:
+    ghost_door = randint(1, 3)
+    print("Перед тобой три двери...")
+    print("За одной из них привидение")
+    print("Какую выберешь ты?")
+    door = input('1, 2, или 3?')
+    door_num = int(door)
